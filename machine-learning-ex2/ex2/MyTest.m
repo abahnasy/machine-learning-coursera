b70b0cafd22fa227579b98e2bfda45973778a05a
Ahmed BAHNASY <ahmed.bahnasy@valeo.com>
2017-05-30 16:45:23 +0200

    Sigmoid function solved

---
+++ machine-learning-ex2/ex2/MyTest.m
@@ -0,0 +1,14 @@
+clear ; close all; clc
+
+%% Load Data
+%  The first two columns contains the exam scores and the third column
+%  contains the label.
+
+data = load('ex2data1.txt');
+X = data(:, [1, 2]); y = data(:, 3);
+
+%% ==================== Part 1: Plotting ====================
+%  We start the exercise by first plotting the data to understand the 
+%  the problem we are working with.
+
+pos = find(y==1);
